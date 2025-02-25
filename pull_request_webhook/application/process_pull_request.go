1cc54e55a83f011fb32819e3a08a663987c1938d
Kevin <moralesperezkevin7@gmail.com>
2025-02-25 14:09:47 -0600

    terminado

--- pull_request_webhook/application/process_pull_request.go
+++ pull_request_webhook/application/process_pull_request.go
@@ -7,13 +7,20 @@ import (
 )
 
 func ProcessPullRequestEvent(rawData []byte) int {
-	var eventPayload value_objects.PullRequestEvent
+	var eventPayload value_objects.PullRequestEventPayload
 
 	if err := json.Unmarshal(rawData, &eventPayload); err != nil {
 		return 403
 	}
 
-	log.Printf("Evento pull request recibido con accion de %s", eventPayload.Action)
-
+	if eventPayload.Action == "closed" {
+		log.Printf("Evento Pull Request recibido: Rama Base=%s, Rama Default='%s',User='%s', Repositorio='%s'", eventPayload.PullRequest.Base.Ref, eventPayload.PullRequest.Head.Ref, eventPayload.PullRequest.User.Name, eventPayload.Repository.FullName)
+	}
+	
 	return 200
 }
+
+// Mostrar hacia dónde se hace el Pull Request
+// Desde dónde se hace el pull request
+// Usuario(username) que realizó el pull request
+// Nombre del repositorio en donde se realizó
--- pull_request_webhook/domain/value_objects/payload_structures.go
+++ pull_request_webhook/domain/value_objects/payload_structures.go
@@ -1,19 +1,41 @@
 package value_objects
 
-type PullRequestEvent struct {
+
+type PullRequestEventPayload struct {
 	Action      string      `json:"action"`
 	PullRequest PullRequest `json:"pull_request"`
+	Repository  Repository  `json:"repository"`
+	Base        Base        `json:"base"`
 }
 
 type PullRequest struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
 	Head  Branch `json:"head"`
-	Base  Branch `json:"base"`
+	Base  Base   `json:"base"`
 	URL   string `json:"url"`
+	User  User   `json:"user"`
 }
 
 type Branch struct {
 	Ref string `json:"ref"`
-	SHA string `json:"sha"`
+	Sha string `json:"sha"`
+}
+
+type Base struct {
+	Ref string `json:"ref"` // Rama base del Pull Request (ej: develop, main)
+	Sha string `json:"sha"`
+}
+
+type Repository struct {
+	FullName string `json:"full_name"`
+	URL      string `json:"url"`
+}
+
+type User struct {
+	Name string `json:"name"`
+	Login string `json:"login"`
+	ID    int    `json:"id"`
+	Type  string `json:"type"`
+	URL   string `json:"url"`
 }
